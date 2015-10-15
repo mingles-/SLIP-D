c2912a1c11747e7bce245c56efac4bfc21b51846
Michael Inglis <michaelinglis93@googlemail.com>
2015-10-15 14:59:45 +0100

    Tests for Register & DB Stuff

--- server/SmartLockServer/tests.py
+++ server/SmartLockServer/tests.py
@@ -3,6 +3,7 @@ import SmartLockServer
 import unittest
 import tempfile
 import json
+import models
 from base64 import b64encode
 
 __author__ = 'mingles'
@@ -14,11 +15,17 @@ class SmartLockTestCase(unittest.TestCase):
         self.db_fd, SmartLockServer.app.config['DATABASE'] = tempfile.mkstemp()
         SmartLockServer.app.config['TESTING'] = True
         self.app = SmartLockServer.app.test_client()
-        # SmartLockServer.init_db()
+        models.User.query.delete()
+        models.Lock.query.delete()
+        models.Role.query.delete()
+        models.role_user.delete()
+        models.user_lock.delete()
+        SmartLockServer.db.session.commit()
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(SmartLockServer.app.config['DATABASE'])
+
 
     def auth_header(self, username, password):
         """
@@ -42,29 +49,47 @@ class SmartLockTestCase(unittest.TestCase):
 
     def test_auth_good(self):
         """ Ensure that good user credentials are accepted - Sam """
+        models.user_datastore.create_user(email="mingles", password="python")
+        SmartLockServer.db.session.commit()
         response = self.app.get('/protected-resource', headers=self.auth_header("mingles", "python"))
         self.assertEqual(200, response.status_code)
 
     def test_open_lock_good(self):
         """Ensure that good user credentials are accepted with the open lock """
+        models.user_datastore.create_user(email="mingles", password="python")
+        SmartLockServer.db.session.commit()
         response = self.app.put('/open/123', headers=self.auth_header("mingles", "python"))
         self.assertEqual(200, response.status_code)
 
     def test_open_lock_bad_id(self):
         """Ensure that good user credentials fail with the an unassociated lock"""
+        models.user_datastore.create_user(email="mingles", password="python")
+        SmartLockServer.db.session.commit()
         response = self.app.get('/open/124', headers=self.auth_header("mingles", "python"))
         self.assertEqual(405, response.status_code)
 
     def test_open_lock_nae_lock(self):
         """Ensure that good user credentials are not accepted without a lock"""
+        models.user_datastore.create_user(email="mingles", password="python")
+        SmartLockServer.db.session.commit()
         response = self.app.get('/open', headers=self.auth_header("mingles", "python"))
         self.assertEqual(404, response.status_code)
 
     def test_open_lock_wrong_login(self):
         """Ensure that bad password, but corect username is rejected with correct lock"""
+        models.user_datastore.create_user(email="mingles", password="python")
+        SmartLockServer.db.session.commit()
         response = self.app.get('/open/123', headers=self.auth_header("mingles", "iscool123"))
         self.assertEqual(405, response.status_code)
 
+    def test_register_success(self):
+        """"""
+        response = self.app.get('/protected-resource', headers=self.auth_header("samsmum@mail.com", "python"))
+        self.assertEqual(403, response.status_code)
+        response = self.app.post('/register', data=dict(email="samsmum@mail.com", password="python"))
+        self.assertEqual(201, response.status_code)
+        response = self.app.get('/protected-resource', headers=self.auth_header("samsmum@mail.com", "python"))
+        self.assertEqual(200, response.status_code)
 
 if __name__ == '__main__':
     unittest.main()
