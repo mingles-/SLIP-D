a484a6ebc9f51a9eed247a833efef59a47351102
Michael Inglis <michaelinglis93@googlemail.com>
2015-10-15 14:57:28 +0100

    Add Close & Register Endpoint

--- server/SmartLockServer/SmartLockServer.py
+++ server/SmartLockServer/SmartLockServer.py
@@ -1,6 +1,6 @@
 from os import environ
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask.ext.httpauth import HTTPBasicAuth
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
@@ -12,13 +12,14 @@ api = Api(app)
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
 
-from models import *
+import models
 
 
 @auth.get_password
-def get_password(username):
-    if username == 'mingles':
-        return 'python'
+def get_password(email):
+    users_with_that_email = models.User.query.filter_by(email=email)
+    if users_with_that_email.count() > 0:
+        return users_with_that_email[0].password
     return None
 
 
@@ -49,6 +50,19 @@ class ProtectedResource(Resource):
         return "Hello", 200
 
 
+class Register(Resource):
+
+    def post(self):
+        email = request.form['email']
+        password = request.form['password']
+        models.user_datastore.create_user(email=email, password=password)
+        db.session.commit()
+
+        new_user = models.User.query.filter_by(email=email)
+
+        return new_user[0].email, 201
+
+
 class CloseLock(Resource):
     """
     This endpoint closes the lock if lockID and associated user is consistent within the database
@@ -62,6 +76,14 @@ class CloseLock(Resource):
         else:
             return "", 403
 
+class LockList(Resource):
+
+    def get(self):
+        locks = db.session.query(models.Lock).all()
+        lock_json = []
+        for lock in locks:
+            lock_json.append({"id": lock.id, "locked": lock.locked})
+        return lock_json
 
 class OpenLock(Resource):
     """
@@ -79,9 +101,13 @@ class OpenLock(Resource):
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(Mingles, '/mingles')
+api.add_resource(LockList, '/lock')
+api.add_resource(Register, '/register')
+
+
 api.add_resource(ProtectedResource, '/protected-resource')
 api.add_resource(OpenLock, '/open/<int:lock_id>')
-api.add_resource(OpenLock, '/close/<int:lock_id>')
+api.add_resource(CloseLock, '/close/<int:lock_id>')
 
 
 if __name__ == '__main__':
