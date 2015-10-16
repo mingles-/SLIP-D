688e0ebfc1b66c41304f1cc8e85f6e72e9b5a62c
Michael Inglis <michaelinglis93@googlemail.com>
2015-10-16 16:56:12 +0100

    Tests Refactored

--- server/SmartLockServer/tests/tests_auth.py
+++ server/SmartLockServer/tests/tests_auth.py
@@ -26,28 +26,6 @@ class SmartLockTestCase(BaseTest):
         response = self.app.get('/protected-resource', headers=self.auth_header("test@mail.com", "python"))
         self.assertEqual(200, response.status_code)
 
-    def test_open_lock_good(self):
-        """Ensure that good user credentials are accepted with the open lock """
-        response = self.app.post('/register-lock/123', headers=self.auth_header("test@mail.com", "python"))
-        self.assertEqual(200, response.status_code)
-        response = self.app.put('/open/123', headers=self.auth_header("test@mail.com", "python"))
-        self.assertEqual(200, response.status_code)
-
-    def test_open_lock_bad_id(self):
-        """Ensure that good user credentials fail with the an non existing lock"""
-        response = self.app.put('/open/124', headers=self.auth_header("test@mail.com", "python"))
-        self.assertEqual(404, response.status_code)
-
-    def test_open_lock_nae_lock(self):
-        """Ensure that good user credentials are not accepted without a lock"""
-        response = self.app.put('/open/', headers=self.auth_header("test@mail.com", "python"))
-        self.assertEqual(404, response.status_code)
-
-    def test_open_lock_wrong_login(self):
-        """Ensure that bad password, but corect username is rejected with correct lock"""
-        response = self.app.put('/open/123', headers=self.auth_header("test@mail.com", "iscool123"))
-        self.assertEqual(401, response.status_code)
-
     def test_register_success(self):
         """Register to gain access to protected resource"""
         response = self.app.get('/protected-resource', headers=self.auth_header("test2@mail.com", "python"))
@@ -57,12 +35,7 @@ class SmartLockTestCase(BaseTest):
         response = self.app.get('/protected-resource', headers=self.auth_header("test2@mail.com", "python"))
         self.assertEqual(200, response.status_code)
 
-    def test_register_lock_success(self):
-        """Register a lock to a user"""
-        response = self.app.post('/register-lock/123', headers=self.auth_header("test@mail.com", "python"))
-        self.assertEqual(200, response.status_code)
-        response = self.app.post('/register-lock/123', headers=self.auth_header("test@mail.com", "python"))
-        self.assertEqual(401, response.status_code)
+
 
 if __name__ == '__main__':
     unittest.main()
