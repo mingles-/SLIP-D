b040f6605b92c6681d6858ae3e430c31c20820b9
Michael Inglis <michaelinglis93@googlemail.com>
2015-11-03 18:12:45 +0000

    Add Many to Many relationship table

--- Project/models.py
+++ Project/models.py
@@ -9,19 +9,17 @@ role_user = db.Table('role_user',
                        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
                        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
-user_lock = db.Table('user_lock',
-                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-                       db.Column('lock_id', db.Integer(), db.ForeignKey('lock.id')),
-                       db.Column('is_owner', db.Boolean()))
 
-
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
+class UserLock(db.Model):
+    __tablename__ = 'user_lock'
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), primary_key=True)
+    lock_id = db.Column(db.Integer(), db.ForeignKey('lock.id'), primary_key=True)
+    is_owner = db.Column(db.Boolean())
+    lock = db.relationship("Lock")
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     first_name = db.Column(db.String(255), unique=True)
@@ -31,12 +29,19 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=role_user,
                             backref=db.backref('users', lazy='dynamic'))
-    # locks = db.relationship('locks', secondary=user_lock,
-    #                         backref=db.backref('locks', lazy='dynamic'))
+    locks = db.relationship("UserLock")
 
 
 
 class Lock(db.Model):
+    __tablename__ = 'lock'
     id = db.Column(db.Integer, primary_key=True)
     locked = db.Column(db.Boolean())
 
+
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
