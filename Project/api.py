aa7615a766bc7fb98e5aebd6f581dac1fd116cae
= <ayrtonmassey@googlemail.com>
2015-11-18 00:50:34 +0000

    Fix lock logic

--- Project/api.py
+++ Project/api.py
@@ -284,15 +284,18 @@ class ImOpen(Resource):
         if database_lock_id.count() > 0:
 
             lock = database_lock_id.first()
-            if lock.requested_open is True and lock.actually_open is False: # Change is requested
-                lock.actually_open = True   # Open the lock
-                lock.requested_open = False # Change has been made
+            if lock.requested_open is True and lock.actually_open is False: # Open is requested, Lock is closed on server, Lock is open
+                lock.actually_open  = True # Open the lock
                 db.session.commit()
                 return lock.actually_open, 202
-            elif lock.requested_open is True and lock.actually_open is True:
-                return lock.actually_open, 200
-            else:
+            elif lock.requested_open is True and lock.actually_open is True: # Open is requested, Lock is open on server, Lock is open
                 return lock.actually_open, 202
+            elif lock.requested_open is False and lock.actually_open is True: # Close is requested, Lock is open on server, Lock is open
+                return lock.actually_open, 200
+            elif lock.requested_open is False and lock.actually_open is False: # Close is requested, Lock is closed on server, Lock is open
+                lock.actually_open = True # Open the lock
+                db.session.commit()
+                return lock.actually_open, 200
         else:
             return False, 404
 
@@ -303,15 +306,18 @@ class ImClosed(Resource):
         if database_lock_id.count() > 0:
 
             lock = database_lock_id.first()
-            if lock.requested_open is True and lock.actually_open is True: # Change is requested
-                lock.actually_open = False   # Open the lock
-                lock.requested_open = False # Change has been made
+            if lock.requested_open is False and lock.actually_open is True: # Close is requested, Lock is open on server, Lock is closed
+                lock.actually_open  = False # Open the lock
                 db.session.commit()
                 return lock.actually_open, 202
-            elif lock.requested_open is True and lock.actually_open is False:
-                return lock.actually_open, 200
-            else:
+            elif lock.requested_open is False and lock.actually_open is False: # Close is requested, Lock is closed on server, Lock is closed
                 return lock.actually_open, 202
+            elif lock.requested_open is True and lock.actually_open is False: # Open is requested, Lock is closed on server, Lock is closed
+                return lock.actually_open, 200
+            elif lock.requested_open is True and lock.actually_open is True: # Open is requested, Lock is open on server, Lock is closed
+                lock.actually_open = False # Open the lock
+                db.session.commit()
+                return lock.actually_open, 200
         else:
             return False, 404
 
---
+++ set_env.sh
@@ -0,0 +1,4 @@
+export SECURITY_PASSWORD_HASH='sha512_crypt'
+export SECURITY_PASSWORD_SALT='auwgd7atfag9023ry'
+export DATABASE_URL='postgres://postgres:postgres@localhost/slip_api'
+export APP_SETTINGS='config.Config'
